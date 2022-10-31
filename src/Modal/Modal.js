f7d91752e80dd4a4514ed292c762ea1385098480
Chelovek A <cheloveka@Cheloveks-MacBook-Pro.local>
2022-10-31 18:52:50 +0300

    some magic with react

---
+++ src/Modal/Modal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Modal.css";
+
+export default class Modal extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={() => this.setState({ isOpen: true })}>
+          Open dialog window
+        </button>
+
+        {this.state.isOpen && (
+          <div className="moadal">
+            <div className="modal-body">
+              <h1>Creative dialog window</h1>
+              <p>I'm awesome</p>
+              <button onClick={() => this.setState({ isOpen: false })}>
+                Close dialog window
+              </button>
+            </div>
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+}
